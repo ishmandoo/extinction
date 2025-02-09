62e1977d6b95ecdc83ad14526c910ea6f0315b0a
dtersegno <dtersegno@gmail.com>
2025-02-09 15:18:54 -0500

    added a gun to the ship

--- main.lua
+++ main.lua
@@ -7,11 +7,54 @@ local fleet = {
     { pos = { x = 300, y = 80 }, vel = { x = 100, y = 0 }, dir = 0, color = { 0, 0, 255 } },
 }
 
+local bullets = {
+}
+
+local text = " "
+
+function love.keypressed( key )
+	fire()
+	-- if key == "space" then
+		-- for i=0,5 do
+			-- table.insert(
+				-- fleet, { pos = { x = 0 + i*10, y = 85 + i * 10}, vel = {x = 20, y =30}, dir = 0, color = { .2, .5, .8 } }
+			-- )
+		-- end
+	-- end
+	-- if key == "a" then
+		-- text = "A-0k!"
+	-- end
+end
+
+function love.keyreleased( key )
+	text = ""
+end
+		
+
+function fire()
+	for i, ship in ipairs(fleet) do
+		for j=0,4 do
+			table.insert(
+				bullets,
+				{ pos = {x = ship.pos.x, y = ship.pos.y}, vel = { x = 2*ship.vel.x, y = 2*ship.vel.y}, dir = 0, color = {0.5, 0.6, 0.9} }
+			)
+		end
+		text = "Bang!"
+	end
+end
+
+
 function love.load()
     love.graphics.setBackgroundColor(0, 0, 0)
 end
 
 function love.draw()
+
+	-- bang!
+	for i, ship in ipairs(fleet) do
+		love.graphics.print( text, ship.pos.x + 25, ship.pos.y + 40 )
+	end
+
     for i, planet in ipairs(planets) do
         love.graphics.setColor(planet.color)
         love.graphics.circle("fill", planet.pos.x, planet.pos.y, planet.radius)
@@ -26,6 +69,11 @@ function love.draw()
         love.graphics.setColor(ship.color)
         love.graphics.circle("fill", ship.pos.x, ship.pos.y, 5)
     end
+	
+	for i, bullet in ipairs(bullets) do
+		love.graphics.setColor(bullet.color)
+		love.graphics.circle("fill", bullet.pos.x, bullet.pos.y, 2)
+	end
 end
 
 function love.update(dt)
@@ -41,6 +89,21 @@ function love.update(dt)
 
             ship.vel.x = ship.vel.x + acc * dx / dist * dt
             ship.vel.y = ship.vel.y + acc * dy / dist * dt
+        end
+    end
+	
+	for i, bullet in ipairs(bullets) do
+        bullet.pos.x = bullet.pos.x + bullet.vel.x * dt
+        bullet.pos.y = bullet.pos.y + bullet.vel.y * dt
+
+        for j, planet in ipairs(planets) do
+            local dx = planet.pos.x - bullet.pos.x
+            local dy = planet.pos.y - bullet.pos.y
+            local dist = math.sqrt(dx * dx + dy * dy)
+            local acc = 1000000 / (dist * dist)
+
+            bullet.vel.x = bullet.vel.x + acc * dx / dist * dt
+            bullet.vel.y = bullet.vel.y + acc * dy / dist * dt
         end
     end
 end
